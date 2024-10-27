e4666cb81c54315f5077f840c8fe00840fbb65d2
TakedaONe <146793652+TakedaONe@users.noreply.github.com>
2024-10-27 08:10:17 -0400

    Delete app directory

--- app/src/main/java/com/example/youtobe/utils/Constants.kt
+++
@@ -1,9 +0,0 @@
-package com.example.youtobe.utils
-
-object Constants {
-
-    const val PART = "snippet,contentDetails"
-    const val MAX_RESULTS = 50
-    const val CHANNEL_ID = "Ds3yZMQhQ7Q"
-
-}
